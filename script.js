b08a9a1cbafb3a943c7b8233bcda2a74edf3e3bc
onwebhacking-ui <onwebhacking@gmail.com>
2026-04-23 10:27:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    let output = '';
-    data.forEach(user => {
-      output += `<p>${user.name} - ${user.age}</p>`;
-    });
-    document.getElementById('output').innerHTML = output;
-  });
